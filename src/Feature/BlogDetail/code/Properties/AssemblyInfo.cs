f8af6cc89363cffcebf9bcc0aede581b3faa4ff1
User <User@DESKTOP-VU1OGKN>
2021-07-02 14:16:17 +0530

    Add project files.

---
+++ src/Feature/BlogDetail/code/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SitecorePoweredBlog.Feature.BlogDetail")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SitecorePoweredBlog.Feature.BlogDetail")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07b978c9-f65d-416f-9b92-dd7c935e8679")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Feature/Search/code/Controllers/SearchIndexController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecorePoweredBlog.Feature.Search.Controllers
+{
+    public class SearchIndexController : Controller
+    {
+        // GET: SearchIndex
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            var item = Sitecore.Context.Database.GetItem("{DBB9D18E-6214-4126-8746-87FC5493771C}");
+            string str = item.Fields["ButtonLabel"].Value;
+            string search = item.Fields["HighlighterText"].Value;
+            // Logic
+            // Debugging - Being The Detective In a Crime Movie Where You Are Also The Murderer.
+            return View(item);
+        }
+    }
+}
